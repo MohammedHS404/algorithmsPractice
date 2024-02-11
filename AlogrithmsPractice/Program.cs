04c7751de326040077d1b0323d579632e597ce9d
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-02-11 23:29:19 +0200

    solution

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,5 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Solution solution = new Solution();
+int[] nums = new int[] { 2, 7, 11, 15 };
+int target = 11;
+int[] result = solution.TwoSum(nums, target);
 
-using System;
-
-Console.WriteLine("Hello, World!");
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int,int> numsSet = new Dictionary<int,int>();
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (numsSet.TryGetValue(complement, out int value))
+            {
+                return new int[] { value, i };
+            }
+            numsSet.Add(nums[i],i);
+        }
+        
+        return new int[] { } ;
+    }
+}
