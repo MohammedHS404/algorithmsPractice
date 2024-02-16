8769d2fafabd914f775c7768b3dc733bf4ca4f09
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-02-17 01:04:11 +0200

    DFS Iterative

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,10 +1,15 @@
-﻿using AlogrithmsPractice;
+﻿
+int?[] array = {3,4,5,-7,-6,null,null,-7,null,-5,null,null,null,-4};
 
-int?[] array = { 3, 9, 20, null, null, 15, 7 };
+BinaryTreeFromArray binaryTree = new BinaryTreeFromArray();
 
-BinaryTree binaryTree = new BinaryTree();
+TreeNode? root = binaryTree.ArrayToTree(array);
 
-TreeNode? root = binaryTree.CreateBinaryTreeFromArray(array);
+Solution solution = new();
+
+int max=solution.MaxDepth(root);
+Console.WriteLine();
+Console.WriteLine($"Depth:{max}");
 
 public class Solution
 {
@@ -15,8 +20,35 @@ public class Solution
             return 0;
         }
 
-        int left = MaxDepth(root.left);
-        int right = MaxDepth(root.right);
-        return 1 + Math.Max(left ,right);
+        Stack<(TreeNode,int)> nextNodes = new Stack<(TreeNode,int)>();
+
+        nextNodes.Push((root,1));
+
+        int maxDepth = 0;
+        
+
+        while (nextNodes.Count > 0)
+        {
+            (TreeNode currentNode,int depth)  = nextNodes.Pop();
+            
+            Console.WriteLine(currentNode.val);
+
+            if (currentNode.right != null)
+            {
+                nextNodes.Push((currentNode.right,depth+1));
+            }
+
+            if (currentNode.left != null)
+            {
+                nextNodes.Push((currentNode.left,depth+1));
+            }
+
+            if (currentNode.left == null && currentNode.right == null)
+            {
+                maxDepth = Math.Max(depth, maxDepth);
+            }
+        }
+
+        return maxDepth;
     }
 }
