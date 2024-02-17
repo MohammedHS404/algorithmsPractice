fbc6f2595031cbd2aa883c6992f28c03ebfebbfe
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-02-18 01:46:25 +0200

    solution

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,6 +1,25 @@
-﻿int?[] array = { 3, 9, 20, null, null, 15, 7 };
+﻿BinaryTreeFromArray binaryTree = new BinaryTreeFromArray();
 
-BinaryTreeFromArray binaryTree = new BinaryTreeFromArray();
+TreeNode? root = binaryTree.ArrayToTree(new int?[] { 1, 3 });
+TreeNode? root2 = binaryTree.ArrayToTree(new int?[] { 1, 2, 3 });
 
-TreeNode? root = binaryTree.ArrayToTree(array);
+Solution solution = new();
+Console.WriteLine(solution.IsSameTree(root, root2));
 
+public class Solution
+{
+    public bool IsSameTree(TreeNode? p, TreeNode? q)
+    {
+        if (p == null && q == null)
+        {
+            return true;
+        }
+
+        if (p?.val != q?.val)
+        {
+            return false;
+        }
+
+        return IsSameTree(p?.left, q?.left) && IsSameTree(p?.right, q?.right);
+    }
+}
