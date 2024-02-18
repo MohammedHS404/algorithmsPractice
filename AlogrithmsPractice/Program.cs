1ee9076a56b13d97a811d6ac13db38689ebdd131
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-02-18 02:31:26 +0200

    def sol

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,6 +1,30 @@
-﻿int?[] array = { 3, 9, 20, null, null, 15, 7 };
+﻿int?[] array = { 4,2,7,1,3,6,9 };
 
 BinaryTreeFromArray binaryTree = new BinaryTreeFromArray();
 
 TreeNode? root = binaryTree.ArrayToTree(array);
+Solution solution = new Solution();
+TreeNode result = solution.InvertTree(root);
+int[] resultArray = BinaryTreeToArray.ConvertToArray(result);
+Console.WriteLine(string.Join(",", resultArray));
 
+public class Solution
+{
+    public TreeNode? InvertTree(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+        
+        InvertTree(root.left);
+        
+        InvertTree(root.right);
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        return root;
+    }
+}
