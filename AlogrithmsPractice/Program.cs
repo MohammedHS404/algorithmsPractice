213f81827c71fd49740188399f020709cc3c7f94
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-02-19 03:12:01 +0200

    sorting

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -4,3 +4,44 @@ BinaryTreeFromArray binaryTree = new BinaryTreeFromArray();
 
 TreeNode? root = binaryTree.ArrayToTree(array);
 
+Solution solution = new Solution();
+
+IList<IList<string>> result = solution.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+
+foreach (var anagrams in result)
+{
+    foreach (var anagram in anagrams)
+    {
+        Console.Write(anagram + " ");
+    }
+
+    Console.WriteLine();
+}
+
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, IList<string>> wordDict = new Dictionary<string, IList<string>>();
+
+        foreach (string str in strs)
+        {
+            char[] charArray = str.ToCharArray();
+            
+            Array.Sort(charArray);
+            
+            string? sortedString = new string(charArray);
+
+            if (wordDict.TryGetValue(sortedString, out IList<string>? anagrams))
+            {
+                anagrams.Add(str);
+            }
+            else
+            {
+                wordDict.Add(sortedString, new List<string> { str });
+            }
+        }
+
+        return wordDict.Values.ToList();
+    }
+}
