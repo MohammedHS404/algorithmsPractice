02787da2dda3a89a68c1d6b58476b96ad4ff976c
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-02-21 20:42:05 +0200

    optimal sol

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,6 +1,31 @@
-﻿int?[] array = { 3, 9, 20, null, null, 15, 7 };
+﻿Solution sol = new Solution();
+Console.WriteLine(sol.IsPalindrome("A man, a plan, a canal: Panama"));
 
-BinaryTreeFromArray binaryTree = new BinaryTreeFromArray();
-
-TreeNode? root = binaryTree.ArrayToTree(array);
-
+public class Solution {
+    public bool IsPalindrome(string s) {
+        if (string.IsNullOrWhiteSpace(s))
+        {
+            return true;
+        }
+        
+        for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+        {
+            while(!char.IsLetterOrDigit(s[i]) && i < j)
+            {
+                i++;
+            }
+            
+            while(!char.IsLetterOrDigit(s[j]) && i < j)
+            {
+                j--;
+            }
+   
+            if (char.ToLower(s[i]) != char.ToLower(s[j]))
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
