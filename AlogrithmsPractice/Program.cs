070ed71116ec4b38d9f793a779b2ea852e2b81ce
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-03-06 04:46:23 +0200

    brute force

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿public class Solution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        List<IList<int>> result = new List<IList<int>>();
+
+        if (nums.Length < 3)
+        {
+            return result;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                for (int k = j + 1; k < nums.Length; k++)
+                {
+                    if (nums[i] + nums[j] + nums[k] == 0)
+                    {
+                        List<int> temp = new List<int>();
+                        temp.Add(nums[i]);
+                        temp.Add(nums[j]);
+                        temp.Add(nums[k]);
+                        temp.Sort();
+                        if (!result.Any(l=>l.SequenceEqual(temp)))
+                        {
+                            result.Add(temp);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
