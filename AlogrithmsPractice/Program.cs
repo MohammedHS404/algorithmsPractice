2410695ae90789cc5e1b64650c24271a73a82a63
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-03-11 06:27:49 +0200

    optimal sol

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿Solution solution = new();
+
+int[] numbers =
+{
+    3, 24, 50, 79, 88, 150, 345
+};
+
+int target = 200;
+
+int[] result = solution.TwoSum(numbers, target);
+
+foreach (var number in result)
+{
+    Console.WriteLine(number);
+}
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int left = 0;
+        int right = numbers.Length - 1;
+
+        while (left < right)
+        {
+            if (numbers[left] + numbers[right] == target)
+            {
+                return new int[]
+                {
+                    left + 1, right + 1
+                };
+            }
+            else if (numbers[left] + numbers[right] > target)
+            {
+                right--;
+            }
+            else if (numbers[left] + numbers[right] < target)
+            {
+                left++;
+            }
+        }
+
+        return new int[2];
+    }
+}
