56528bf441be2445ecc4216489a6b36e08e185cb
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-03-17 05:02:07 +0200

    O(n) solution

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+
+        int l = 0;
+        int r = height.Length - 1;
+
+        int maxArea = 0;
+
+        while (l < r)
+        {
+            int xDiff = Math.Abs(r - l);
+
+            int area = Math.Min(height[l], height[r]) * xDiff;
+
+            maxArea = Math.Max(maxArea, area);
+
+            if (height[l] < height[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+
+        return maxArea;
+    }
+}
