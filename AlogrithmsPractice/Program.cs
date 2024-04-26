01ea20e24c66ff1e2d70a4c96d2b630a946deeef
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-04-26 07:32:24 +0300

    optimal

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,70 @@
-﻿
+﻿Solution solution = new Solution();
+
+Console.WriteLine(solution.IsValid("){")); // True
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        if (s.Length % 2 != 0)
+        {
+            return false;
+        }
+
+        Dictionary<char, string> types = new Dictionary<char, string>
+        {
+            {
+                '(', "open"
+            },
+            {
+                '[', "open"
+            },
+            {
+                '{', "open"
+            },
+            {
+                ')', "close"
+            },
+            {
+                ']', "close"
+            },
+            {
+                '}', "close"
+            }
+        };
+
+        Dictionary<char, char> pairs = new Dictionary<char, char>
+        {
+            {
+                '(', ')'
+            },
+            {
+                '[', ']'
+            },
+            {
+                '{', '}'
+            }
+        };
+
+        Stack<char> stack = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i]=='(' || s[i]=='[' || s[i]=='{')
+            {
+                stack.Push(s[i]);
+            }
+            else if (stack.Count == 0 || pairs[stack.Pop()] != s[i])
+            {
+                return false;
+            }
+        }
+
+        if (stack.Count == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
