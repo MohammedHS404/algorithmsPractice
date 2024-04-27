efe6668823083919c9c18a0c7ee8cf06cd1c4244
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-04-27 10:47:42 +0300

    brute solution

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿string test = "cabababababababd";
+
+Console.WriteLine(LongestPalindrome(test));
+
+string LongestPalindrome(string s)
+{
+    if (s.Length == 0)
+    {
+        return "";
+    }
+
+    string longest = "";
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        for (int j = i + 1; j <= s.Length; j++)
+        {
+            string sub = s.Substring(i, j - i);
+            
+            if (IsPalindromic(sub) && sub.Length > longest.Length)
+            {
+                longest = sub;
+            }
+        }
+    }
+    
+    return longest;
+}
+
+bool IsPalindromic(string s)
+{
+
+    int l = 0;
+    int r = s.Length - 1;
+
+    while (l < r)
+    {
+        if (s[l] != s[r])
+        {
+            return false;
+        }
+        l++;
+        r--;
+    }
+
+    return true;
+}
