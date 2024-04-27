288f487d6ba3db918381fab627ce7a0c0056d6a3
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-04-27 15:00:57 +0300

    optimal solution

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿Solution solution = new Solution();
+Console.WriteLine(solution.RomanToInt("VI"));
+
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> roman = new Dictionary<char, int>()
+        {
+            {
+                'I', 1
+            },
+            {
+                'V', 5
+            },
+            {
+                'X', 10
+            },
+            {
+                'L', 50
+            },
+            {
+                'C', 100
+            },
+            {
+                'D', 500
+            },
+            {
+                'M', 1000
+            }
+        };
+
+        int result = 0;
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            int number = roman[s[i]];
+
+            if (i < s.Length - 1)
+            {
+                int nextNumber = roman[s[i + 1]];
+
+                if (number < nextNumber)
+                {
+                    result -= number;
+                    continue;
+                }
+            }
+
+            result += number;
+        }
+
+        return result;
+    }
+}
