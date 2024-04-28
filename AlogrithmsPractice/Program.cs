0aeb66594a512fb5163d3741f105d43af3bc6788
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-04-28 23:05:43 +0300

    optimal sol

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿Solution solution = new Solution();
+
+int?[] array = new int?[]
+{
+    1, 2, 2, null, 3, null, 3
+};
+
+TreeNode root = new BinaryTreeFromArray().ArrayToTree(array);
+
+bool result = solution.IsSymmetric(root);
+
+Console.WriteLine(result);
+
+
+public class Solution
+{
+    public bool IsSymmetric(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+
+        if (root.left == null && root.right == null)
+        {
+            return true;
+        }
+
+        return IsSymmetricDfs(root.left, root.right);
+    }
+
+    static bool IsSymmetricDfs(TreeNode? left, TreeNode? right)
+    {
+        if (left == null && right == null)
+        {
+            return true;
+        }
+
+        if (left?.val != right?.val)
+        {
+            return false;
+        }
+
+        return IsSymmetricDfs(left.left, right.right) && IsSymmetricDfs(left.right, right.left);
+    }
+}
