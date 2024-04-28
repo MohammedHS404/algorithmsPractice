b032f65ab924b186b89c3ce2aa5a744d14fea4e3
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-04-29 00:18:32 +0300

    optimal solution

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿int?[] arr = new int?[]
+{
+    2,1,3
+};
+
+TreeNode root = new BinaryTreeFromArray().ArrayToTree(arr);
+
+bool result = new Solution().IsValidBST(root);
+
+Console.WriteLine(result);
+
+public class Solution
+{
+    public bool IsValidBST(TreeNode? root)
+    {
+        return Helper(root).Item1;
+    }
+
+    (bool, long, long) Helper(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return (true, long.MinValue, long.MaxValue);
+        }
+        
+        (bool leftValid, long leftMax, long leftMin) = Helper(root.left);
+
+        if (!leftValid || leftMax >= root.val)
+        {
+            return (false, 0, 0);
+        }
+
+        (bool rightValid, long rightMax, long rightMin) = Helper(root.right);
+
+        if (!rightValid || rightMin <= root.val)
+        {
+            return (false, 0, 0);
+        }
+
+        return (true, Math.Max(rightMax, root.val), Math.Min(leftMin, root.val));
+    }
+
+}
