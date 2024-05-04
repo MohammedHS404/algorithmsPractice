07ce5415e1be9a7099a2ec1ba6cc2c6a9dc29a0c
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-05-04 20:02:08 +0300

    sol

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,46 @@
-﻿Console.WriteLine();
+﻿Solution solution = new Solution();
+
+string[] tokens = new string[]
+{
+    "10","6","9","3","+","-11","*","/","*","17","+","5","+"
+};
+
+Console.WriteLine(solution.EvalRPN(tokens));
+
+public class Solution
+{
+    public int EvalRPN(string[] tokens)
+    {
+        Stack<int> stack = new Stack<int>();
+
+        foreach (string token in tokens)
+        {
+            if (token == "+")
+            {
+                stack.Push(stack.Pop() + stack.Pop());
+            }
+            else if (token == "-")
+            {
+                int a = stack.Pop();
+                int b = stack.Pop();
+                stack.Push(b - a);
+            }
+            else if (token == "*")
+            {
+                stack.Push(stack.Pop() * stack.Pop());
+            }
+            else if (token == "/")
+            {
+                int a = stack.Pop();
+                int b = stack.Pop();
+                stack.Push(b / a);
+            }
+            else
+            {
+                stack.Push(int.Parse(token));
+            }
+        }
+        
+        return stack.Pop();
+    }
+}
