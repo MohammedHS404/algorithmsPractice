3711fe18dcfe6eaabca2f626fe8955dd379c35a1
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-08-05 05:29:23 +0300

    optimal

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine();
+﻿Console.WriteLine(URLify("Mr John Smith    ".ToCharArray(), 13)); // True
+
+string? URLify(char[] str, int trueLength)
+{
+    int index = str.Length-1;
+    for (int i = trueLength - 1; i >= 0; i--)
+    {
+        if (str[i] != ' ')
+        {
+            str[index] = str[i];
+            index--;
+        }
+        else
+        {
+            str[index] = '0';
+            str[index - 1] = '2';
+            str[index - 2] = '%';
+            index -= 3;
+        }
+    }
+
+    return new string(str);
+}
