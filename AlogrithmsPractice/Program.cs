d1b5bf640227a08edcf06a676f251f6ee6f74a96
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-08-05 05:27:03 +0300

    latest

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine();
+﻿Solution solution = new Solution();
+
+int[] temperatures = new int[]
+{
+    75, 71, 69, 72, 76
+};
+
+int[] result = solution.DailyTemperatures(temperatures);
+
+foreach (int item in result)
+{
+    Console.WriteLine(item);
+}
+
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        int[] result = new int[temperatures.Length];
+
+        Stack<int> stack = new Stack<int>();
+
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            while (stack.Count > 0 && temperatures[stack.Peek()] < temperatures[i])
+            {
+                int index = stack.Pop();
+                result[index] = i - index;
+            }
+
+            stack.Push(i);
+        }
+
+        return result;
+    }
+}
