ee3a93269dc70620c634550bf317514567aa4842
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-08-12 06:09:29 +0300

    using new matrix

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine();
+﻿
+int[][] matrix = new int[][]
+{
+    new int[] {1, 2, 3},
+    new int[] {4, 5, 6},
+    new int[] {7, 8, 9}
+};
+
+int[][] r = RotateMatrix(matrix);
+
+foreach (var row in r)
+{
+    Console.WriteLine(string.Join(", ", row));
+}
+
+
+int[][] RotateMatrix(int[][] matrix)
+{
+    int[][] result = new int[matrix.Length][];
+    
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        result[i] = new int[matrix.Length];
+    }
+    
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        for (int j = 0; j < matrix.Length; j++)
+        {
+            result[j][matrix.Length - 1 - i] = matrix[i][j];
+        }
+    }
+    
+    return result;
+}
