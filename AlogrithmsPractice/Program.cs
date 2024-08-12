c8bc4b8a2fee3356d16eee648a7ec8e93262032d
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-08-12 03:07:32 +0300

    insertion, replacement methods

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -6,49 +6,78 @@ Console.WriteLine(OneEditAway("pale", "bale")); // True
 
 Console.WriteLine(OneEditAway("pale", "bake")); // False
 
+Console.WriteLine(OneEditAway("abc", "bc")); // True
+
+Console.WriteLine(OneEditAway("abc", "ac")); // True
+
+Console.WriteLine(OneEditAway("abc", "ab")); // True
+
+Console.WriteLine(OneEditAway("abc", "a")); // False
+
 bool OneEditAway(string s1, string s2)
 {
-    int count = 0;
+    int diff = s1.Length - s2.Length;
 
-    if (Math.Abs(s1.Length - s2.Length) > 1)
+    if (diff > 1 || diff < -1)
     {
         return false;
     }
 
-    Dictionary<char, int> s1CharCount = new Dictionary<char, int>();
-
-    for (int i = 0; i < s1.Length; i++)
+    if (diff == 0)
     {
-        if (s1CharCount.ContainsKey(s1[i]))
-        {
-            s1CharCount[s1[i]]++;
-        }
-        else
-        {
-            s1CharCount.Add(s1[i], 1);
-        }
+        return OneEditAway_ReplacedCharacter(s1, s2);
     }
 
-    for (int i = 0; i < s2.Length; i++)
+    if (diff == 1)
     {
-        if (s1CharCount.ContainsKey(s2[i]))
-        {
-            s1CharCount[s2[i]]--;
-        }
+        return OneEditAway_InsertedCharacter(s1, s2);
     }
-    
-    foreach (var item in s1CharCount)
+
+    if (diff == -1)
     {
-        if (item.Value != 0)
+        return OneEditAway_InsertedCharacter(s2, s1);
+    }
+
+    return true;
+}
+
+bool OneEditAway_InsertedCharacter(string larger, string smaller)
+{
+    int largerIndex = 0;
+    int smallerIndex = 0;
+
+    while (largerIndex < larger.Length && smallerIndex < smaller.Length)
+    {
+        if (larger[largerIndex] != smaller[smallerIndex])
         {
-            count++;
-            
-            if (count > 1)
+            if (largerIndex != smallerIndex)
             {
                 return false;
             }
+
+            largerIndex++;
+        }
+        else
+        {
+            largerIndex++;
+            smallerIndex++;
         }
     }
 
     return true;
+}
+
+bool OneEditAway_ReplacedCharacter(string s1, string s2)
+{
+    int count = 0;
+
+    for (int i = 0; i < s1.Length; i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            count++;
+        }
+    }
+
+    return count == 1;
 }
