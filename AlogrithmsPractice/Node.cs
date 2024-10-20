d859c6a820502c36bc54e1396c968644ee5e3e01
mohamed.shokry <mohamed.shokry@flairstech.com>
2024-10-20 06:08:14 +0300

    adding node class

--- AlogrithmsPractice/Node.cs
+++ AlogrithmsPractice/Node.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace AlogrithmsPractice;
 
 public class Node<T>
@@ -37,5 +39,38 @@ public class Node<T>
 
             current = current.Next;
         }
+    }
+
+    public override string ToString()
+    {
+        Node<T>? current = this;
+        StringBuilder sb = new StringBuilder();
+
+        while (current != null)
+        {
+            sb.Append(current.Data);
+            sb.Append(" -> ");
+            current = current.Next;
+        }
+        
+        // Remove the last " -> "
+        
+        sb.Remove(sb.Length - 4, 4);
+
+        return sb.ToString();
+    }
+    
+    public static Node<T> FromArray(T[] array)
+    {
+        Node<T> head = new Node<T>(array[0]);
+        Node<T> current = head;
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            current.Next = new Node<T>(array[i]);
+            current = current.Next;
+        }
+
+        return head;
     }
 }
