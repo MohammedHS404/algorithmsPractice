ebbf714f8d2637902a24c1f61986de1c17d667f5
MohammedHS404 <bobohany44@gmail.com>
2024-10-31 12:18:20 +0300

    n + log(n)

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,11 +1,45 @@
-﻿public class Solution
+﻿Solution solution = new Solution();
+
+// [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+
+int[][] matrix = new int[3][];
+
+matrix[0] = new int[] { 1, 3, 5, 7 };
+matrix[1] = new int[] { 10, 11, 16, 20 };
+matrix[2] = new int[] { 23, 30, 34, 60 };
+
+int target = 31;
+
+bool result = solution.SearchMatrix(matrix, target);
+
+Console.WriteLine(result);
+
+
+public class Solution
 {
-    public int Search(int[] nums, int target)
+    public bool SearchMatrix(int[][] matrix, int target)
     {
+        int arrayIndex = -1;
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            if (matrix[i][0] <= target && matrix[i][matrix[i].Length - 1] >= target)
+            {
+                arrayIndex = i;
+                break;
+            }
+        }
+
+        if (arrayIndex == -1)
+        {
+            return false;
+        }
+
+        int[] nums = matrix[arrayIndex];
         int start = 0;
         int end = nums.Length - 1;
 
-        return BinarySearch(nums, start, end, target);
+        return BinarySearch(nums, start, end, target) > -1;
     }
 
     private int BinarySearch(int[] arr, int start, int end, int target)
