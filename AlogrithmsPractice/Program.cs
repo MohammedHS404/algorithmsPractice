b7883a6fe078e18cdb6a6f30a496cf7527235e6d
mhs404 <mohammedhs404@gmail.com>
2024-11-09 20:00:17 +0200

    optimal solution

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine();
+﻿Console.WriteLine(new Solution().FindMin(new int[] { 1 }));
+
+public class Solution
+{
+    public int FindMin(int[] nums)
+    {
+        return FindMin(nums, 0, nums.Length - 1);
+    }
+
+    int FindMin(int[] nums, int left, int right)
+    {
+        if (left == right)
+        {
+            return nums[left];
+        }
+
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] > nums[right])
+        {
+            return FindMin(nums, mid + 1, right);
+        }
+        else
+        {
+            return FindMin(nums, left, mid);
+        }
+    }
+}
