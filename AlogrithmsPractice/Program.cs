826db9289038bcd5ae27e8c64ed525d6588afb60
mhs404 <mohammedhs404@gmail.com>
2024-11-09 20:47:03 +0200

    initial

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine();
+﻿
+Solution sol = new Solution();
+
+int[] prices = [2, 1, 3, 4];
+
+int result = sol.MaxProfit(prices);
+
+Console.WriteLine(result);
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int max = 0;
+
+        for (int i = prices.Length - 1; i > 0; i--)
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+                int profit = prices[i] - prices[j];
+                if (profit > max)
+                {
+                    max = profit;
+                }
+            }
+        }
+
+        return max;
+    }
+}
