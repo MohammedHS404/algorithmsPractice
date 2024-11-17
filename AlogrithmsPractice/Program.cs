51926203c3044b4f5acd9ef70b34a07398d0c306
mhs404 <mohammedhs404@gmail.com>
2024-11-17 17:38:04 +0200

    best I can do :(

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine();
+﻿
+Solution solution = new Solution();
+
+Console.WriteLine(solution.LengthOfLongestSubstring("bbbbadsbd")); // 3
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+
+        HashSet<char> set = new HashSet<char>();
+
+        int max = 0;
+
+        int l = 0, r = 0;
+
+        while (r < s.Length)
+        {
+            if (set.Contains(s[r]))
+            {
+                max = Math.Max(max, r - l);
+                set.Remove(s[l]);
+                l++;
+            }
+            else
+            {
+                set.Add(s[r]);
+                r++;
+            }
+        }
+
+        return Math.Max(max, r - l);
+    }
+}
