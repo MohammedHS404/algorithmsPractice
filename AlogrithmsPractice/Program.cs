5f98f421c3bed93ef97dc917db6f35d9438779fb
mhs404 <mohammedhs404@gmail.com>
2024-11-19 03:44:42 +0200

    noice

--- AlogrithmsPractice/Program.cs
+++ AlogrithmsPractice/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine();
+﻿Solution solution = new Solution();
+
+Console.WriteLine(solution.LongestRepeatingCharacter("ABAB"));
+Console.WriteLine(solution.LongestRepeatingCharacter("AABABBA"));
+Console.WriteLine(solution.LongestRepeatingCharacter("ABABABABA"));
+Console.WriteLine(solution.LongestRepeatingCharacter("ABCD"));
+Console.WriteLine(solution.LongestRepeatingCharacter("AAAA"));
+Console.WriteLine(solution.LongestRepeatingCharacter("AAABBB"));
+Console.WriteLine(solution.LongestRepeatingCharacter("AAABBBAA"));
+
+public class Solution {
+    public int LongestRepeatingCharacter(string s) {
+        int left = 0, right = 0, max = 0;
+
+        char current = s[0];
+
+        while (right < s.Length) {
+            if (s[right] == current) {
+                right++;
+                max = Math.Max(max, right - left);
+            } else {
+                left = right;
+                current = s[right];
+            }
+        }
+
+        return max;
+    }
+}
